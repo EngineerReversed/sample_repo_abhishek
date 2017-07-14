b9f386cea8dfbe27c9581c6e68478442d72969fe
EngineerReversed <abhishek.shakya88@gmail.com>
2017-07-14 12:40:15 +0530

    'Calculator'

--- Python_Day3/calculator.py
+++ Python_Day3/calculator.py
@@ -1,3 +1,16 @@
+import logging
+#--log=INFO
+
+logging.basicConfig(filename='example.log',level=logging.DEBUG)
+logger=logging.getLogger(__name__)
+logging.info('Start reading records')
+records={'john':55, 'tom':66}
+#logging.warning('Warning out!')
+logging.debug('Records: %s',records)
+'''numeric_level = getattr(logging, loglevel.upper(),None)
+if not isinstance(numeric_level,int):
+    raise ValueError('Invalid log level: %s'%loglevel)
+'''
 
 def menu():
     l = ["Select operation.","1.Add","2.Subtract","3.Multiply","4.Divide","5.Modulus"]
@@ -5,14 +18,38 @@ def menu():
         print(i)
     st='y';
     while(st=='y'):
-        choice = input("Enter choice(1/2/3/4/5):")
+        flag0=True
+        while flag0:
+            choice = raw_input("Enter choice(1/2/3/4/5):")
+            try:    
+                choice=int(choice)
+                flag0=False
+            except ValueError:
+                print("Please try again ")
+
         print choice    
-        num1 = input("Enter first number: ")
-        num2 = input("Enter second number: ")
+        flag=True
+        while flag:
+            num1 = raw_input("Enter first number: ")
+            try:    
+                num1=int(num1)
+                flag=False
+            except ValueError:
+                print("Please try again ")
+
+        flag1=True
+        while flag1:
+            num2 = raw_input("Enter second number: ")
+            try:
+                num2=int(num2)
+                flag1=False
+            except ValueError:
+                print("Please try again ")
+
         if choice == 1:
             print(num1+num2)
         elif choice == 2:
-            if x>y:
+            if num1>num2:
                 print(num1-num2)
             else:
                 print("num2 is Greater than num1.Thus displayed result is mod of the difference:")
